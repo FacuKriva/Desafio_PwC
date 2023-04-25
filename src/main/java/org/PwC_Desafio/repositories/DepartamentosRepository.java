bf102dcbd44a7430b4ac599d995491497574791b
FacuKriva <facukriva2001@gmail.com>
2023-04-25 20:04:45 -0300

    Se modifican los nombres de singular a plural para mejor organización

--- src/main/java/org/PwC_Desafio/repositories/DepartamentosRepository.java
+++ src/main/java/org/PwC_Desafio/repositories/DepartamentosRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartamentoRepository extends JpaRepository<Departamentos, Long> {
+public interface DepartamentosRepository extends JpaRepository<Departamentos, Long> {
 }
