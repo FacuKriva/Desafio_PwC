cde118d1ea6f103c49cb13dcd9845afbac67abe2
FacuKriva <facukriva2001@gmail.com>
2023-04-26 12:54:31 -0300

    Renombramos Main.java a FacultadApp.java

---
+++ src/main/java/org/PwC_Desafio/FacultadApp.java
@@ -0,0 +1,12 @@
+package org.PwC_Desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultadApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FacultadApp.class, args);
+
+    }
+}
--- src/main/java/org/PwC_Desafio/Main.java
+++
@@ -1,7 +0,0 @@
-package org.PwC_Desafio;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
