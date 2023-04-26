d171f5699522bb3ce5607cf3801144b9e9b303df
FacuKriva <facukriva2001@gmail.com>
2023-04-25 22:31:03 -0300

    Modificamos el repositorio para permitir buscar a todos los profesores en un departamento específico

--- src/main/java/org/PwC_Desafio/repositories/DepartamentosRepository.java
+++ src/main/java/org/PwC_Desafio/repositories/DepartamentosRepository.java
@@ -1,9 +1,13 @@
 package org.PwC_Desafio.repositories;
 
 import org.PwC_Desafio.model.Departamentos;
+import org.PwC_Desafio.model.Profesores;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepartamentosRepository extends JpaRepository<Departamentos, Long> {
+    List<Profesores> listarProfesoresPorDepartamento(Long idDepartamento);
 }
