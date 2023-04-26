6963bfbb4c4eda12890a35d5ba5560f680fe7ccf
FacuKriva <facukriva2001@gmail.com>
2023-04-26 13:22:41 -0300

    Creamos varios profesores para poder listarlos

--- src/test/java/org/PwC_Desafio/service/impl/ProfesoresServiceTest.java
+++ src/test/java/org/PwC_Desafio/service/impl/ProfesoresServiceTest.java
@@ -25,12 +25,37 @@ public class ProfesoresServiceTest {
         profesores.setDni("12.345.678");
         profesores.setEstadoCivil("Casado");
         profesores.setDepartamentoAsignado(null);
+
+        Profesores profesores2 = new Profesores();
+        profesores2.setNombre("John");
+        profesores2.setApellido("Doe");
+        profesores2.setDni("99.345.678");
+        profesores2.setEstadoCivil("Casado");
+        profesores2.setDepartamentoAsignado(null);
+
+        Profesores profesores3 = new Profesores();
+        profesores3.setNombre("Jane");
+        profesores3.setApellido("Doe");
+        profesores3.setDni("99.543.897");
+        profesores3.setEstadoCivil("Casada");
+        profesores3.setDepartamentoAsignado(null);
+
+        Profesores profesores4 = new Profesores();
+        profesores4.setNombre("Joaquin");
+        profesores4.setApellido("Saviola");
+        profesores4.setDni("28.478.625");
+        profesores4.setEstadoCivil("Soltero");
+        profesores4.setDepartamentoAsignado(null);
+
     }
 
     @Test
     @Order(2)
     public void asignarDepartamento() {
-        Profesores profesores = profesoresService.asignarDepartamento(1L, 11L);
+        Profesores profesores = profesoresService.asignarDepartamento(1L, 300L);
+        Profesores profesores2 = profesoresService.asignarDepartamento(2L, 200L);
+        Profesores profesores3 = profesoresService.asignarDepartamento(3L, 300L);
+        Profesores profesores4 = profesoresService.asignarDepartamento(4L, 100L);
     }
 
     //-----------------------------------------------------------------------------------------------
@@ -38,7 +63,7 @@ public class ProfesoresServiceTest {
     @Test
     @Order(3)
     public void cambiarDepartamento() {
-        Profesores profesores = profesoresService.cambiarDepartamento(1L, 22L);
+        Profesores profesores = profesoresService.cambiarDepartamento(1L, 100L);
     }
     //-----------------------------------------------------------------------------------------------
 
