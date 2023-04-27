a02dfe49df93e6c96b1b3fe31c8f085926fc711e
FacuKriva <facukriva2001@gmail.com>
2023-04-27 05:58:39 -0300

    Se borran archivos

--- src/main/java/org/PwC_Desafio/repositories/SeccionRepository.java
+++
@@ -1,9 +0,0 @@
-package org.pwc_desafio.repositories;
-
-import org.pwc_desafio.model.Seccion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeccionRepository extends JpaRepository<Seccion, Long> {
-}
